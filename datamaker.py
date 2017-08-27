cad0813c93594593e0ab82a762ad63a3b1add0cc
Thibault Clérice <leponteineptique@gmail.com>
2017-08-27 12:54:26 +0200

    Directories makers + URNs provided by @alisonbabeu

---
+++ datamaker.py
@@ -0,0 +1,47 @@
+from csv import reader
+import os
+import shutil
+
+textgroups = []
+works = []
+with open("urns.csv") as f:
+    csv = reader(f, delimiter="\t")
+    for line in csv:
+        textgroup, work, _, _, _, path, tgname, workname, _ = tuple(line)
+        if textgroup == "URN Author":
+            continue
+        print(textgroup)
+        # If the author is anonymous, the textgroup == the work
+
+        if tgname.lower().startswith("anony"):
+            tgname = workname
+        # Create textgroup
+        if textgroup not in textgroups:
+            if not os.path.exists("data/{}".format(textgroup)):
+                os.makedirs("data/{}".format(textgroup))
+                with open("data/{}/__cts__.xml".format(textgroup), "w") as tg:
+                    tg.write("""<textgroup xmlns="http://chs.harvard.edu/xmlns/cts" urn="urn:cts:latinLit:{}">
+    <groupname xml:lang="lat">{}</groupname>
+</textgroup>""".format(textgroup, tgname))
+        if not work in works:
+            if not os.path.exists("data/{}/{}".format(textgroup, work)):
+                os.makedirs("data/{}/{}".format(textgroup, work))
+                with open("data/{}/{}/__cts__.xml".format(textgroup, work), "w") as wk:
+                    wk.write("""<work xmlns="http://chs.harvard.edu/xmlns/cts"
+  groupUrn="urn:cts:latinLit:{tg}"
+  urn="urn:cts:latinLit:{tg}.{wk}"
+  xml:lang="lat"
+>
+    <title xml:lang="eng">{workname}</title>
+    <edition 
+      workUrn="urn:cts:latinLit:{tg}.{wk}" 
+      urn="urn:cts:latinLit:{tg}.{wk}.digilibLT-lat1">
+        <label xml:lang="lat">{workname}</label>
+        <description xml:lang="lat">Edition from DigilibLT {digilibLT}</description>
+    </edition>
+</work>""".format(tg=textgroup, wk=work, workname=workname, digilibLT=path.split("/")[1]))
+        else:
+            print("Work {} is already annotated".format(work))
+
+        # Copy the file
+        shutil.copyfile(path, "data/{tg}/{wk}/{tg}.{wk}.digilibLT-lat1".format(tg=textgroup, wk=work))
